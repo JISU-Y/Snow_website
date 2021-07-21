32798b5962ec6f900534ba714bb413fb02c50d93
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2021-07-21 21:38:20 +0900

    js: navbar buttons moving to each link

--- script.js
+++ script.js
@@ -3,10 +3,44 @@ const navbarHeight = navbar.getBoundingClientRect().height;
 const btnNavbars = document.querySelector(".nav-menu-bars");
 
 document.addEventListener("scroll", () => {
-  console.log(navbarHeight);
   if (window.scrollY > navbarHeight) {
     navbar.classList.add("white");
   } else {
     navbar.classList.remove("white");
   }
 });
+
+// nav bar li에 있는 버튼들 이동
+const navbarButtons = document.querySelectorAll(".nav-menu li");
+
+navbarButtons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+
+    if (link == null) {
+      return;
+    }
+
+    scrollIntoView(link);
+  });
+});
+
+// Home의 아래 버튼 (about으로 감)
+const btnGotoAbout = document.querySelector(".down-btn");
+
+btnGotoAbout.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+
+  if (link == null) {
+    return;
+  }
+
+  scrollIntoView(link);
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
